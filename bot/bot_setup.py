a17d25d6d7183889d542ad59060fce91bd705e38
Annovid <nil.nil532@gmail.com>
2024-12-07 19:50:49 +0300

    add feedback

--- bot/bot_setup.py
+++ bot/bot_setup.py
@@ -1,5 +1,5 @@
 from bot.bot_context import bot_context
-from bot.handlers import general, friends, tournaments, admin
+from bot.handlers import general, friends, tournaments, admin, feedback
 from bot.handlers.answer_to_message import manager as answer_to_message_manager
 from utils.custom_logger import logger
 
@@ -10,5 +10,7 @@ general.register_handlers(bot_context)
 friends.register_handlers(bot_context)
 tournaments.register_handlers(bot_context)
 admin.register_handlers(bot_context)
+feedback.register_handlers(bot_context)
+# Last most common handler
 answer_to_message_manager.register_handlers(bot_context)
 logger.info('All handlers registered successfully.')
